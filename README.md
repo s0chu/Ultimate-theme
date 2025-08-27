e0ea0971c20f72314911098f8e9feb5ab78ce9cf
s0chu <didididi328@gmail.com>
2025-08-27 13:23:33 +0300

    Finished

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Ultimate-theme
 My setup for a brand new Linux install
 
-DE: Gnome
-Theme: [Orchis](https://github.com/vinceliuice/Orchis-theme?tab=readme-ov-file)
-Terminal: zsh (Kali like)
-Background: black solid color
-Top-panel: autohide
-Dock: autohide
+##Summary
+-DE: Gnome
+-Theme: [Orchis](https://github.com/vinceliuice/Orchis-theme?tab=readme-ov-file)
+-Terminal: zsh (Kali like)
+-Background: black solid color
+-Top-panel: autohide
+-Dock: autohide 
+
+###Extensions settings
+-[Hide Top Bar](https://extensions.gnome.org/extension/545/hide-top-bar/)
+-Ubuntu-dock / Dock already existent in Extensions 
+
+![Terminal](image.png)
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+#Theme
+sudo apt install -y git
+git clone https://github.com/vinceliuice/Orchis-theme
+cd Orchis-theme
+./install.sh -n "Ultimate" -t green -c dark -i ubuntu -l -f --tweaks solid compact black dock 
+cd ..
+
+#ZSH
+sudo apt install -y zsh
+sudo apt install -y zsh-syntax-highlighting 
+chsh -s $(which zsh)
+reboot
+cat zsh.config > ~/.zshrc
+source ~/.zshrc
+
+#Background
+gsettings set org.gnome.desktop.background picture-uri "file://$(realpath background.jpeg)"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$(realpath background.jpeg)"
+
+#Extensions
+sudo apt install -y gnome-tweaks
+sudo apt install -y gnome-shell-extension-prefs
